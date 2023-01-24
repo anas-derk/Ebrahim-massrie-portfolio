40fb3a7b98b6448ac793cd354118ddc2c6db9cf9
anas derk <anas.derk2020@gmail.com>
2023-01-24 06:03:58 -0800

    Start Programming My Terminal

--- src/Pages/Terminal/index.jsx
+++ src/Pages/Terminal/index.jsx
@@ -13,6 +13,12 @@ const Terminal = ({ pageTitle }) => {
 
     const [error, setError] = useState("");
 
+    // const user_name = localStorage.getItem("user-name");
+
+    const [isOpenTerminal, setIsOpenTerminal] = useState(false);
+
+    const [isTerminalOpening, setIsTerminalOpening] = useState(false);
+
     const openTerminal = () => {
         if (!userName) {
             setError("Error, Please Enter Your Name !!!");
@@ -22,7 +28,13 @@ const Terminal = ({ pageTitle }) => {
             }, 3000);
         }
         else {
-            localStorage.setItem("user-name", userName);
+            // localStorage.setItem("user-name", userName);
+            setIsTerminalOpening(true);
+            let openTerminalTimeout = setTimeout(() => {
+                setIsTerminalOpening(false);
+                setIsOpenTerminal(true);
+                clearTimeout(openTerminalTimeout);
+            }, 2000);
         }
     }
 
@@ -44,10 +56,14 @@ const Terminal = ({ pageTitle }) => {
                     <button className="btn btn-success p-3 w-100" onClick={openTerminal}>Open Terminal</button>
                 </div>
             </div>
-            {error && <p className="alert alert-danger">{ error }</p>}
-            <div className="terminal-control-box p-4">
+            {error && <p className="alert alert-danger">{error}</p>}
+            {isTerminalOpening && <div class="d-flex align-items-center bg-secondary p-3 loading-box">
+                <strong>Loading...</strong>
+                <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+            </div>}
+            {isOpenTerminal && <div className="terminal-control-box p-4">
                 aa
-            </div>
+            </div>}
         </div>
         // End Terminal
     );
