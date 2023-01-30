4af5a235f2991d75ffc32e4d214490220bcb8024
anas derk <anas.derk2020@gmail.com>
2023-01-30 02:49:25 -0800

    Remove Command Helper

--- src/Pages/Terminal/index.jsx
+++ src/Pages/Terminal/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./index.min.css";
 import { useNavigate } from "react-router-dom";
+import TerminalData from "./terminal_data";
 
 const Terminal = ({ pageTitle }) => {
 
@@ -23,34 +24,6 @@ const Terminal = ({ pageTitle }) => {
     const [previousCommandsList, setPreviousCommandsList] = useState([]);
 
     const [commandIndex, setCommandIndex] = useState(0);
-
-    let useStatmentsAsTableData = [
-        <td>1. First: If You Use Terminal For First Time, Please Enter Your Name Then Click On Open Terminal</td>,
-        <td>2. You Can Running Commands By Write In Terminal Area The Next Command Then Click On Enter . <span className="run-command-statment p-2 bg-secondary m-2 d-block">emt [commandName] --[parameterName] [value] </span></td>,
-        <td>3. For Display All Of Commands, Please Write Command: <span className="get-all-commands-statement bg-secondary p-2 m-2 d-block">emt get --all-commands</span></td>,
-        <td>4. This Terminal it is not Characters Case Sensitive (example: get is equal GET)</td>,
-        <td>5. For Knowledge Details Of Determinated Command Please Write Command: <span className="command-help bg-secondary p-2 m-2 d-block">emt [commandName] --help</span></td>,
-        <td>6. Last, I Wish For You Fantastic Experince With My Terminal .</td>
-    ];
-
-    let useStatmentsAsTerminalCommandResults = [
-        "1. If You Use Terminal For First Time, Please Enter Your Name Then Click On Open Terminal",
-        "2. You Can Running Commands By Write In Terminal Area The Next Command Then Click On Enter: emt [commandName] --[parameterName] [value]",
-        "3. For Display All Of Commands, Please Write Command: emt get --all-commands",
-        "4. This Terminal it is not Characters Case Sensitive (example: get is equal GET)",
-        "5. For Knowledge Details Of Determinated Command Please Write Command: emt [commandName] --help",
-        "6. Last, I Wish For You Fantastic Experince With My Terminal .",
-    ];
-
-    let allCommands = [
-        "emt cls",
-        "emt who-am-i",
-        "emt get --previous-commands",
-        "emt clear --previous-commands-list",
-        "emt close",
-        "emt restart",
-        "emt set --user-name [new_user_name] (Without Square Bracktes)",
-    ];
 
     const addCommandToPreviousCommandList = () => {
         if (!previousCommandsList.includes(command)) {
@@ -153,13 +126,13 @@ const Terminal = ({ pageTitle }) => {
                 break;
             }
             case "emt get --all-commands": {
-                setResults(allCommands);
+                setResults(TerminalData.allCommands);
                 setCommandIndex(0);
                 addCommandToPreviousCommandList();
                 break;
             }
             case "emt get --use-statments": {
-                setResults(useStatmentsAsTerminalCommandResults);
+                setResults(TerminalData.useStatmentsAsTerminalCommandResults);
                 setCommandIndex(0);
                 addCommandToPreviousCommandList();
                 break;
@@ -195,7 +168,6 @@ const Terminal = ({ pageTitle }) => {
                             document.location.reload();
                         }, 1500);
                     }, 2500);
-
                 } else {
                     setResults([
                         "Error, The Command Is Not Found !!",
@@ -265,7 +237,7 @@ const Terminal = ({ pageTitle }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {useStatmentsAsTableData.map((statement, index) =>
+                        {TerminalData.useStatmentsAsTableData.map((statement, index) =>
                             <tr key={index}>
                                 {statement}
                             </tr>
