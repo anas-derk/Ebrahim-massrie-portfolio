c6d4146243f029ebfb6c57d956985e1f46c5fbc7
anas derk <anas.derk2020@gmail.com>
2023-03-10 10:33:22 -0800

    Start Design My Skills Page

--- src/Pages/MySkills/index.jsx
+++ src/Pages/MySkills/index.jsx
@@ -2,6 +2,8 @@ import Header from "../../Components/Header/index";
 import { Fragment } from "react";
 import { useEffect } from "react";
 import "./index.min.css";
+import { FaFacebookF } from 'react-icons/fa';
+import { MdArrowDropUp } from "react-icons/md";
 
 const MySkills = ({ pageTitle }) => {
 
@@ -20,14 +22,45 @@ const MySkills = ({ pageTitle }) => {
             <div className="my-skills pt-5">
                 {/* Start Container */}
                 <div className="container">
-                    <h1 className="page-name text-center">My Skills</h1>
+                    <h1 className="page-name text-center mb-5">My Skills</h1>
                     {/* Start Grid System */}
                     <div className="row">
                         {/* Start Column */}
                         <div className="col-md-6">
                             {/* Start Grid System */}
-                            <div className="row">
-                                
+                            <div className="row align-items-center skill-box pt-3 pb-3 bg-secondary">
+                                {/* Start Column */}
+                                <div className="col-md-1">
+                                    <FaFacebookF className="skill-icon" />
+                                </div>
+                                {/* End Column */}
+                                {/* Start Column */}
+                                <div className="col-md-10 mb-4">
+                                    <h5 className="">Front End developer</h5>
+                                    <h6 className="mb-0">More Than 4 years</h6>
+                                </div>
+                                {/* End Column */}
+                                {/* Start Column */}
+                                <div className="col-md-1">
+                                    <MdArrowDropUp className="skill-details-open-arrow" />
+                                </div>
+                                {/* End Column */}
+                                {/* Start Skill Details Box */}
+                                <div className="skill-details-box col-md-12">
+                                    <table className="w-100">
+                                        <thead>
+                                            <tr>
+                                                <th>Tech Number</th>
+                                                <th>Tech Name</th>
+                                                <th>Level</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            
+                                        </tbody>
+                                    </table>
+                                </div>
+                                {/* End Skill Details Box */}
                             </div>
                             {/* End Grid System */}
                         </div>
